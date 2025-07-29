2c03a9edd7f38a62c1ff9428a5a2b1bd11ed4320
GarrixA <aphrogarrix10@gmail.com>
2025-07-29 11:10:09 +0200

    updated workflow

--- CLA/main.py
+++ CLA/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main_app():
-    return "<h1>Welcome to trendsaf core<h1/>"
+    return "<h1>Welcome to trendsaf core!<h1/>"
 
 if __name__== "__main__":
     serve(app, host="0.0.0.0", port=8080)
